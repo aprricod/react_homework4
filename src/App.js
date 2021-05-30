76a0f8f7e8dfe3cc594e31c3cbd5ec9e870d7a71
Artem <aprricod@gmail.com>
2021-05-31 00:48:41 +0300

    второй компонент завершен

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import Comp1 from "./Comps/Comp1";
+import Comp2 from "./Comps/Comp2";
 
 function App() {
   return (
     <div>
+      <p>Классовый (ООП) подход</p>
       <Comp1 />
+      <hr />
+      <p>Функциональный подход</p>
+      <Comp2 />
     </div>
   );
 }
--- src/Comps/Comp2.js
+++ src/Comps/Comp2.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import { useState } from "react";
+import React from "react";
 
-export default function Comp2() {
-    return (
-        <div>
-            
-        </div>
-    )
+function Comp2() {
+  const [count, setcount] = useState(0);
+  const [comment, setcomment] = useState([]);
+
+  let myRef = React.createRef();
+
+  let handler = () => {
+    let currentCount = count;
+    currentCount++;
+    setcount(currentCount);
+  };
+
+  let addComment = () => {
+    let commentValue = myRef.current.value;
+    let comments = [...comment, commentValue];
+    setcomment(comments);
+    myRef.current.value = "";
+  };
+
+  return (
+    <div>
+      <h1>State</h1>
+      <div>
+        <button onClick={handler}>Изменение state</button>
+      </div>
+      <div>{count % 2 === 0 ? "even" : "odd"}</div>
+      <div>{count} раз</div>
+      <div>
+        <textarea ref={myRef}></textarea>
+      </div>
+      <div>
+        <button onClick={addComment}>Add comment</button>
+      </div>
+      <div>
+        <ul>
+          {comment.map((item, index) => (
+            <li key={index.toString()}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
+export default Comp2;
