033f965638fd4a92b0b7d61f48848f858f36fe06
Artem <aprricod@gmail.com>
2021-06-04 23:47:18 +0300

    рабочая версия + установлен material

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Scooters from "./Scooters";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      scooters: [
+        {
+          markder: false,
+          name: "Xiaomi Mi Electric Scooter Pro 2",
+          chargeLevel: 34,
+          costPerHour: 1.4,
+          img: "https://cdn.alza.sk/ImgW.ashx?fd=f16&cd=MSXI12a4",
+        },
+        {
+          markder: false,
+          name: "Xiaomi Mi Electric Scooter 1S EU",
+          chargeLevel: 53,
+          costPerHour: 6.4,
+          img: "https://cdn.alza.sk/ImgW.ashx?fd=f16&cd=MSXI12a2",
+        },
+        {
+          markder: false,
+          name: "Street Surfing VOLTAIK MGT 350",
+          chargeLevel: 34,
+          costPerHour: 1.4,
+          img: "https://cdn.alza.sk/ImgW.ashx?fd=f16&cd=SPTepic083",
+        },
+        {
+          markder: false,
+          name: "Ninebot eKickscooter ZINGE8",
+          chargeLevel: 53,
+          costPerHour: 6.4,
+          img: "https://cdn.alza.sk/ImgW.ashx?fd=f16&cd=NINE32",
+        },
+      ],
+    };
+  }
+
+  handleMarked(name) {
+    const scooters = this.state.scooters.concat();
+    const scooter = scooters.find((c) => c.name === name);
+    scooter.marked = !scooter.marked;
+    this.setState({
+      scooters: scooters,
+    });
+    console.log(name);
+  }
+
+  renderScooter() {
+    return this.state.scooters.map((scooters) => {
+      return (
+        <Scooters
+          scooters={scooters}
+          key={scooters.name + Math.random()}
+          onMark={this.handleMarked.bind(this, scooters.name)}
+        />
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="list">{this.renderScooter()}</div>
+      </div>
+    );
+  }
 }
-
-export default App;
